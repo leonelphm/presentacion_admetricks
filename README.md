72820413ef6f8b8c7e17fdc6cb9ee95fc90c46c4
Leonel Paolo Hernandez Macchiarulo <leonelphm@gmail.com>
2021-10-11 14:06:00 -0430

    Generada presentacion para Admetricks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# presentacion_admetricks
-Tarea Técnica Data Capture Engineer Admetricks
+# Presentacion Admetricks
+
+Presentación interactiva de la tarea Técnica de Captura de Datos Admetricks
+
---
+++ js/routers_contents.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {   
+    $("#portada").load("contenido/0_portada/index.html");
+    $("#introduccion").load("contenido/1_introduccion/index.html"); 
+    $("#task").load("contenido/2_task/index.html");
+	$("#desarrollo").load("contenido/3_desarrollo/index.html"); 
+	$("#desarrollo_2").load("contenido/3_desarrollo/parte2.html"); 
+	$("#desarrollo_3").load("contenido/3_desarrollo/parte3.html");
+	$("#pn_1").load("contenido/6_codigo/index.html");
+});
